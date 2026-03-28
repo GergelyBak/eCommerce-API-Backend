7a2c69cde41a1ff89f482116224a1990588497e4
Gergely Bak <bakgergely@gmail.com>
2026-03-28 14:18:05 +0100

    2nd

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,23 @@
-console.log('Basic Node + TS scaffolding');
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./db";
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Start server ONLY after DB is connected
+const startServer = async () => {
+  await connectDB();
+
+  app.listen(process.env.PORT || 3000, () => {
+    console.log("Server running");
+  });
+};
+
+startServer();
