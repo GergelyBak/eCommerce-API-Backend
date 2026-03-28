b788b2860486d7c5059c73f457e8d19d6dd033a2
Gergely Bak <bakgergely@gmail.com>
2026-03-28 14:25:08 +0100

    zod and middleware

---
+++ src/middleware/validate.ts
@@ -0,0 +1,15 @@
+import type { Request, Response, NextFunction } from 'express';
+import type { ZodTypeAny } from 'zod';
+
+export const validate =
+  (schema: ZodTypeAny) => (req: Request, res: Response, next: NextFunction) => {
+    try {
+      schema.parse(req.body);
+      next();
+    } catch (error: any) {
+      res.status(400).json({
+        message: 'Validation error',
+        errors: error.errors,
+      });
+    }
+  };
